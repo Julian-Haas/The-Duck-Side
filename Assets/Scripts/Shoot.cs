7bb83e7abd7e9422388534e6c651ba6ae1dc99d4
ScorpeAKANils <90929557+ScorpeAKANils@users.noreply.github.com>
2022-11-22 18:17:09 +0100

    CharachterStuff
    Prototype Charachter

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    //spawn Point der Bullet
+   [SerializeField] Transform Gun;
+    //bullet Object 
+    [SerializeField]GameObject Bullet;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //abfragen ob Linke Maustaste gedrückt wurde
+        if (Input.GetButton("Fire1"))
+        {
+            shoot(); 
+        }   
+    }
+
+    private void shoot()
+    {
+        //spawnen der Bullet
+        var bullet = Instantiate(Bullet, Gun.position, Gun.rotation);
+        //bullet flug direction geben 
+        bullet.GetComponent<Rigidbody>().AddForce(Gun.right*10f, ForceMode.VelocityChange);
+    }
+}
